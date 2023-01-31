02df9ca92015a1f2466cfdcc9e84e3498c9eaa40
juncalserrano27 <122307470+juncalserrano27@users.noreply.github.com>
2023-01-30 23:41:57 -0600

    Add files via upload
    Cambio de if por switch, el uso de while y de break

--- programa.java
+++ programa.java
@@ -3,12 +3,15 @@ public class programa {
 
 	public static void main(String[] args) {
 		Scanner scan= new Scanner (System.in);
+		while (true) {
 		System.out.println("¡¡¡Bienvenido!!!");
 		System.out.println("1. Cuadrado");
 		System.out.println("2. Circulo");
+		System.out.println("3. Salir");
 		System.out.println("Indica con un numero cual de las anteriores opciones quieres crear:");
 		int opcion =scan.nextInt();
-		if (opcion==1) {
+		switch (opcion) {
+		case 1: 
 			CUADRADO uno=new CUADRADO();
 			System.out.println("Indique de que longitud sera el lado del cuadrado:");
 			double lado =scan.nextDouble();
@@ -18,7 +21,53 @@ public class programa {
 			System.out.println("3. Calcular el área");
 			System.out.println("Elija lo que desea calcular:");
 			int opcionDos =scan.nextInt();
-			if (opcionDos==1) {
+			switch(opcionDos) {
+			case 1: 
+				System.out.println("La diagonal vale "+uno.calculaDiagonalCu()+"");
+				break;
+			case 2: 
+				System.out.println("El perimetro es "+uno.calcularPerimetroCu()+"");
+				break;
+			case 3:
+				System.out.println("El área es "+uno.calculaAreaCu()+"");
+				break;
+			default:
+				System.out.println("Ingresa un opción valida");
+				break;
+			}
+			break;
+		case 2:
+			CIRCULO unoC=new CIRCULO();
+			System.out.println("Indique de que longitud sera el radio del circulo:");
+			double radio =scan.nextDouble();
+			unoC.cambiarRadio(radio);
+			System.out.println("1. Calcular circunferencia");
+			System.out.println("2. Calcular el área");
+			System.out.println("Elija lo que desea calcular:");
+			int opcionDoss =scan.nextInt();
+			switch (opcionDoss) {
+			case 1: 
+				System.out.println("La circunferencia es "+unoC.calcularCircunferencia()+"");
+				break;
+			case 2: 
+				System.out.println("El área es "+unoC.calcularArea()+"");
+				break;
+			default:
+				System.out.println("Ingresa un opción valida");
+				break;}
+			break;
+		case 3:
+			System.out.println("Saliendo...");
+			System.exit(0);
+			break;
+		default: 
+			System.out.println("Ingresa un opción valida");
+			break;
+		}
+		
+		}
+		
+		/*	if (opcionDos==1) {
 				System.out.println("La diagonal vale "+uno.calculaDiagonalCu()+"");
 			}if (opcionDos==2) {
 				System.out.println("El perimetro es "+uno.calcularPerimetroCu()+"");
@@ -26,25 +75,17 @@ public class programa {
 				System.out.println("El área es "+uno.calculaAreaCu()+"");
 			}else {
 				
-			}
+			}*/
 			
-		}if (opcion==2) {
-		CIRCULO unoC=new CIRCULO();
-		System.out.println("Indique de que longitud sera el radio del circulo:");
-		double radio =scan.nextDouble();
-		unoC.cambiarRadio(radio);
-		System.out.println("1. Calcular circunferencia");
-		System.out.println("2. Calcular el área");
-		System.out.println("Elija lo que desea calcular:");
-		int opcionDos =scan.nextInt();
+		/*
 		if (opcionDos==1) {
 			System.out.println("La circunferencia es "+unoC.calcularCircunferencia()+"");
 		}if (opcionDos==2) {
 			System.out.println("El área es "+unoC.calcularArea()+"");
 		}else {
 			
-		}
+		}*/
 	}
 
 }
-}
+
